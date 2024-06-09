6e043abf672bb943fbe89820c8bbb79430b82271
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-06-09 19:44:23 +0300

    routing structure has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
-import Home from "src/pages/Home";
+import { lazy, Suspense } from "react";
+
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+const Home = lazy(() => import("src/pages/Home"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Container>
+        <Suspense fallback={<div>loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </Container>
+    ),
+    children: [
+      { element: <Home />, index: true },
+      { path: "html", element: <h1>HTML</h1> },
+      { path: "css", element: <h1>css</h1> },
+      { path: "javascript", element: <h1>javascript</h1> },
+      { path: "accessibility", element: <h1>accessibility</h1> },
+      { path: "*", element: <div>Not found</div> },
+    ],
+  },
+]);
 
 export default function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
+}
+
+function Container({ children }) {
+  return (
+    <div className="min-h-screen bg-no-repeat bg-cover bg-[url('/assets/images/pattern-background-desktop-light.svg')]">
+      <header>header</header>
+      <main>{children}</main>
+    </div>
+  );
 }
