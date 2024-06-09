55c0ec02420e8d085f3150c2b85025c4fbb6275b
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-06-09 17:04:12 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Home(props) {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+Home.propTypes = {};
+
+export default Home;
