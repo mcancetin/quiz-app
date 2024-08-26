4af83ac139f981f5444d4dd34ba3f175796cca9b
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-08-26 22:46:48 +0300

    refactor: Simplify Card component classNames usage

--- src/components/common/card/Card.jsx
+++ src/components/common/card/Card.jsx
@@ -3,10 +3,7 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 
 function Card({ children, className, ...props }) {
-  const cardClasses = classNames(
-    "p-3 lg:px-4 lg:py-[20px] bg-pure-white dark:bg-steel-blue rounded-xl",
-    className
-  );
+  const cardClasses = classNames("p-3 lg:px-4 lg:py-[20px] bg-pure-white dark:bg-steel-blue rounded-xl", className);
 
   return (
     <div className={cardClasses} {...props}>
