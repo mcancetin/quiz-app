7a51ae6d20c423cdb7c2fabc8c48d67bbd73df01
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-08-26 22:57:41 +0300

    chore: Update Button component hover styles

--- src/components/common/button/Button.jsx
+++ src/components/common/button/Button.jsx
@@ -7,7 +7,7 @@ function Button({ children, onClick, ...props }) {
   const buttonClasses = classNames(
     "relative cursor-pointer text-pure-white bg-bright-violet heading-sm rounded-xl p-5 w-full shadow-[0_16px_40px_0px_rgba(143, 160, 193, 0.14)] disabled:bg-bright-violet/50 disabled:cursor-not-allowed",
     {
-      "hover:after:bg-pure-white/50 hover:after:absolute hover:after:top-0 hover:after:left-0 hover:after:w-full hover:after:h-full": true,
+      "hover:after:bg-pure-white/50 hover:after:rounded-xl hover:after:absolute hover:after:top-0 hover:after:left-0 hover:after:w-full hover:after:h-full": true,
     }
   );
 
